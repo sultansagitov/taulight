c8e1e173b4cc0cc0ea171464a2054a71312df10d
rizl <sultan03092006@gmail.com>
2025-01-02 21:20:33 +0500

    EchoMessage using JSONMessage

--- src/main/java/net/result/taulight/messages/types/EchoMessage.java
+++ src/main/java/net/result/taulight/messages/types/EchoMessage.java
@@ -1,14 +1,19 @@
 package net.result.taulight.messages.types;
 
 import net.result.sandnode.messages.IMessage;
+import net.result.sandnode.messages.JSONMessage;
 import net.result.sandnode.messages.util.Headers;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 
 import static net.result.taulight.messages.TauMessageTypes.ECHO;
 
-public class EchoMessage extends TextMessage {
+public class EchoMessage extends JSONMessage {
+    public final String data;
+
     public EchoMessage(@NotNull Headers headers, @NotNull String data) {
-        super(headers.setType(ECHO), data);
+        super(headers.setType(ECHO), new JSONObject().put("content", data));
+        this.data = data;
     }
 
     public EchoMessage(@NotNull String data) {
@@ -17,5 +22,6 @@ public class EchoMessage extends TextMessage {
 
     public EchoMessage(IMessage request) {
         super(request);
+        this.data = getContent().getString("content");
     }
 }
