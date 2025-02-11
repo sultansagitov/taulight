c0df2e1ca35c07ed4da78fef30bea39a994b3f31
rizl <sultan03092006@gmail.com>
2025-02-12 00:11:16 +0500

    ChatMessage converted to class

--- src/main/java/net/result/taulight/db/ChatMessage.java
+++ src/main/java/net/result/taulight/db/ChatMessage.java
@@ -1,6 +1,74 @@
 package net.result.taulight.db;
 
-import java.time.ZonedDateTime;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record ChatMessage(String id, String chatID, String content, ZonedDateTime ztd, String memberID) {
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
+public class ChatMessage {
+    @JsonProperty
+    private String id;
+    @JsonProperty
+    private String chatID;
+    @JsonProperty
+    private String content;
+    @JsonProperty
+    private ZonedDateTime ztd;
+    @JsonProperty
+    private String memberID;
+
+    @SuppressWarnings("unused")
+    public ChatMessage() {}
+
+    public ChatMessage(String id, String chatID, String content, ZonedDateTime ztd, String memberID) {
+        this.id = id;
+        this.chatID = chatID;
+        this.content = content;
+        this.ztd = ztd;
+        this.memberID = memberID;
+    }
+
+    public String id() {
+        return id;
+    }
+
+    public String chatID() {
+        return chatID;
+    }
+
+    public String content() {
+        return content;
+    }
+
+    public ZonedDateTime ztd() {
+        return ztd;
+    }
+
+    public String memberID() {
+        return memberID;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ChatMessage) obj;
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.chatID, that.chatID) &&
+                Objects.equals(this.content, that.content) &&
+                Objects.equals(this.ztd, that.ztd) &&
+                Objects.equals(this.memberID, that.memberID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, chatID, content, ztd, memberID);
+    }
+
+    @Override
+    public String toString() {
+        return "<ChatMessage id=%s, chatID=%s, content=%s, ztd=%s, memberID=%s>"
+                .formatted(id, chatID, content, ztd, memberID);
+    }
+
 }
